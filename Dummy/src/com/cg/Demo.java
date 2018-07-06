f9940a656a6a74ec6c2cb6528cd187a805c3ad4b
learning <learning@DIN66007302.corp.capgemini.com>
2018-07-06 17:32:10 +0530

    This is a dummy code.

---
+++ Dummy/src/com/cg/Demo.java
@@ -0,0 +1,12 @@
+package com.cg;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello\n");
+System.out.println("Welcome there!");
+System.out.println("***********");
+	}
+
+}
